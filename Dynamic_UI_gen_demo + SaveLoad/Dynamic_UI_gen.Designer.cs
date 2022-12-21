20e5d40fcd801ac87c4c4858a3bc2e979c9b785d
sirapopModel <91926484+sirapopModel@users.noreply.github.com>
2022-12-21 20:15:15 +0700

    Add files via upload

---
+++ Dynamic_UI_gen_demo + SaveLoad/Dynamic_UI_gen.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Dynamic_UI_gen_demo // This one is in the program name "Dynamic_UI_gen_demo"
+{
+    partial class Dynamic_UI_gen 
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null; // defined a variable
+        // "System.ComponentModel.IContainer is class used to defined component .
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            // disposed == released
+            // if the resource want to dispose a resouce and the components of Form1 isn't null ---> released a task 
+            if (disposing && (components != null)) 
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing); // release the others resource that Form1.cs used 
+                                     // if the others is null ---> return False
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Generate = new System.Windows.Forms.Button();
+            this.SAVE_Button = new System.Windows.Forms.Button();
+            this.NumGen_Box = new System.Windows.Forms.TextBox();
+            this.List_Count = new System.Windows.Forms.TextBox();
+            this.LOAD = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Generate
+            // 
+            this.Generate.BackColor = System.Drawing.Color.Green;
+            this.Generate.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Generate.ForeColor = System.Drawing.Color.Transparent;
+            this.Generate.Location = new System.Drawing.Point(1345, 146);
+            this.Generate.Margin = new System.Windows.Forms.Padding(6);
+            this.Generate.Name = "Generate";
+            this.Generate.Size = new System.Drawing.Size(206, 87);
+            this.Generate.TabIndex = 0;
+            this.Generate.Text = "Play";
+            this.Generate.UseVisualStyleBackColor = false;
+            this.Generate.Click += new System.EventHandler(this.Generate_Click);
+            // 
+            // SAVE_Button
+            // 
+            this.SAVE_Button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.SAVE_Button.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.SAVE_Button.ForeColor = System.Drawing.Color.Transparent;
+            this.SAVE_Button.Location = new System.Drawing.Point(1345, 245);
+            this.SAVE_Button.Margin = new System.Windows.Forms.Padding(6);
+            this.SAVE_Button.Name = "SAVE_Button";
+            this.SAVE_Button.Size = new System.Drawing.Size(206, 87);
+            this.SAVE_Button.TabIndex = 1;
+            this.SAVE_Button.Text = "SAVE";
+            this.SAVE_Button.UseVisualStyleBackColor = false;
+            this.SAVE_Button.Click += new System.EventHandler(this.Save_Button_Click);
+            // 
+            // NumGen_Box
+            // 
+            this.NumGen_Box.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.NumGen_Box.Location = new System.Drawing.Point(1298, 31);
+            this.NumGen_Box.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.NumGen_Box.Name = "NumGen_Box";
+            this.NumGen_Box.Size = new System.Drawing.Size(286, 93);
+            this.NumGen_Box.TabIndex = 2;
+            this.NumGen_Box.TextChanged += new System.EventHandler(this.NumGen_Box_TextChanged);
+            // 
+            // List_Count
+            // 
+            this.List_Count.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.List_Count.Location = new System.Drawing.Point(1298, 454);
+            this.List_Count.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.List_Count.Name = "List_Count";
+            this.List_Count.Size = new System.Drawing.Size(286, 93);
+            this.List_Count.TabIndex = 9;
+            // 
+            // LOAD
+            // 
+            this.LOAD.BackColor = System.Drawing.Color.Gold;
+            this.LOAD.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LOAD.ForeColor = System.Drawing.Color.Transparent;
+            this.LOAD.Location = new System.Drawing.Point(1345, 344);
+            this.LOAD.Margin = new System.Windows.Forms.Padding(6);
+            this.LOAD.Name = "LOAD";
+            this.LOAD.Size = new System.Drawing.Size(206, 87);
+            this.LOAD.TabIndex = 10;
+            this.LOAD.Text = "LOAD";
+            this.LOAD.UseVisualStyleBackColor = false;
+            this.LOAD.Click += new System.EventHandler(this.LOAD_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 19.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(1154, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 71);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "SIZE";
+            // 
+            // Dynamic_UI_gen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1653, 981);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LOAD);
+            this.Controls.Add(this.List_Count);
+            this.Controls.Add(this.NumGen_Box);
+            this.Controls.Add(this.SAVE_Button);
+            this.Controls.Add(this.Generate);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Dynamic_UI_gen";
+            this.Text = "Dynamic_Ui_gen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button Generate;
+        private Button SAVE_Button;
+        private TextBox NumGen_Box;
+        private TextBox List_Count;
+        private Button LOAD;
+        private Label label1;
+    }
+}
---
+++ Dynamic_UI_gen_demo + SaveLoad/Dynamic_UI_gen.cs
@@ -0,0 +1,332 @@
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Linq;
+using System.CodeDom.Compiler;
+using System.Data.Common;
+using System.Runtime.ExceptionServices;
+using System.Windows.Forms;
+
+namespace Dynamic_UI_gen_demo
+{
+    public partial class Dynamic_UI_gen : Form
+    {
+        public Dynamic_UI_gen() // a Method name "Form1" this one will be run once a program is launch.
+        {
+            InitializeComponent();
+        }
+
+        int k = 0;
+        bool checker = false;
+        List<Button> Button_list = new List<Button>();
+        string[][] button_array;
+        string[] checked_array_X;
+        string[] checked_array_O;
+        bool Fist_checked = true;
+        int limiter;
+        void button_click(object sender, EventArgs e) // check winner
+        {
+            Button temp = (Button)sender;
+            string[] index = temp.Name.Split(",");
+            int btn_row = Int32.Parse(index[0]);
+            int btn_col = Int32.Parse(index[1]);
+            if (temp.Text == "")
+            {
+                if (checker == false)
+                {
+                    temp.Text = "X";
+                    button_array[btn_row][btn_col] = "X";
+                    checker = true;
+                }
+                else
+                {
+                    temp.Text = "O";
+                    button_array[btn_row][btn_col] = "O";
+                    checker = false;
+                }
+                //string bt_name = temp.Name ;
+
+                List_Count.Text = temp.Name;
+                check_score(btn_row, btn_col);
+            }
+
+        }
+
+        void check_score(int bt_row, int bt_col)
+        {
+
+            //horizontal
+            for (int Row = 0; Row < k; Row++)
+            {
+                if (button_array[Row].SequenceEqual(checked_array_X))
+                {
+                    MessageBox.Show("X is winner (horizontal line)", "Congratulation", MessageBoxButtons.OK);
+                    jaxed_array_clear(button_array);
+                    return;
+                }
+                else if (button_array[Row].SequenceEqual(checked_array_O))
+                {
+                    MessageBox.Show("O is winner (horizontal line)", "Congratulation", MessageBoxButtons.OK);
+                    jaxed_array_clear(button_array);
+                    return;
+
+                }
+            }
+
+            //vertical
+            for (int Column = 0; Column < k; Column++)
+            {
+                if (button_array[0][Column] == button_array[1][Column])
+                {
+                    for (int Row = 1; Row < k; Row++)
+                    {
+                        if (button_array[Row][Column] != button_array[0][Column] || button_array[Row][Column] == "")
+                        {
+                            break;
+                        }
+                        else if (Row == k - 1)
+                        {
+
+                            MessageBox.Show(button_array[0][Column] + " is winner (vertical line)", "Congratulation", MessageBoxButtons.OK);
+                            jaxed_array_clear(button_array);
+                            return;
+                        }
+
+                    }
+                }
+
+            }
+            // 2 diagonal line  
+
+            for (int i = 0; i < k; i++)
+            {
+                if (button_array[0][0] != button_array[i][i] || button_array[i][i] == "")
+                {
+                    break;
+                }
+                else if (i == (k - 1))
+                {
+                    MessageBox.Show(button_array[0][0] + " is winner (diagonal line)", "Congratulation", MessageBoxButtons.OK);
+                    jaxed_array_clear(button_array);
+                    return;
+                }
+
+            }
+            // slope + diagonal line
+            for (int i = 0; i < k; i++)
+            {
+                if (button_array[k - 1][0] != button_array[k - 1 - i][i] || button_array[k - 1 - i][i] == "")
+                {
+                    break;
+                }
+                else if (i == (k - 1))
+                {
+                    MessageBox.Show(button_array[k - 1][0] + " is winner (diagonal line)", "Congratulation", MessageBoxButtons.OK);
+                    jaxed_array_clear(button_array);
+                    return;
+                }
+            }
+
+        }
+        void jaxed_array_clear(string[][] temp)
+        {
+            for (int i = 0; i < temp.Length; i++)
+            {
+                Array.Clear(temp[i]);
+            }
+        }
+
+
+
+        void Generate_Fuction()
+        {
+            try
+            {
+                k = Int32.Parse(NumGen_Box.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Interger Only", "Alert", MessageBoxButtons.OK);
+                return;
+            }
+            button_array = new string[k][];
+            checked_array_X = new string[k];
+            checked_array_O = new string[k];
+
+            for (int i = 0; i < k; i++)
+            {
+                button_array[i] = new string[k];
+                checked_array_X[i] = "X";
+                checked_array_O[i] = "O";
+            }
+
+            if (Fist_checked == false)
+            {
+                for (int i = 1; i <= limiter; i++)
+                {
+                    this.Controls.Remove(this.Button_list[i - 1]);
+                }
+
+                if (Button_list.Count != 0)
+                {
+                    Button_list.RemoveRange(0, limiter); // (Start index , Amount of removed element ) 
+                }
+            }
+            else
+            {
+                Fist_checked = false;
+            }
+            limiter = k * k;
+
+            int square_button_size = (800 / k);
+            for (int Row = 0; Row < k; Row++)
+            {
+                for (int Column = 0; Column < k; Column++)
+                {
+                    Button temp = new Button();
+                    temp.Name = (Row.ToString() + "," + Column.ToString());
+                    temp.Text = "";
+                    temp.BackColor = Color.DarkGray;
+                    temp.Margin = new Padding(3, 3, 3, 3);
+                    temp.Size = new Size(square_button_size, square_button_size);
+                    temp.Location = new Point(100 + square_button_size * (Column), 100 + square_button_size * (Row));
+                    temp.AutoSize = true;
+                    Button_list.Add(temp);
+                    this.Controls.Add(temp);
+                    temp.Click += new System.EventHandler(this.button_click); //Add event of button (this one is Click)
+                    temp.ForeColor = Color.White;
+                    button_array[Row][Column] = temp.Name;
+                    temp.Font = new Font("Areial", 100 / k);
+                }
+
+            }
+        }
+        private void Generate_Click(object sender, EventArgs e) // Widget Method Call 
+        {
+            Generate_Fuction();
+        }
+
+        
+
+        private void Save_Button_Click(object sender, EventArgs e)
+        {
+            DialogResult Ans = MessageBox.Show("Do you want to save this game?","Alert!",MessageBoxButtons.YesNo);
+            if (Ans == DialogResult.Yes)
+            {
+                SaveFileDialog win = new SaveFileDialog();
+                win.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                win.InitialDirectory = "C:\\Users\\sirapophuangwilai\\Desktop\\My_Dotnet\\Dynamic_UI_gen_demo\\SAVE";
+                win.FilterIndex = 2;
+                win.RestoreDirectory = true;
+                if (win.ShowDialog() == DialogResult.OK )
+                {
+                    Stream fileStream = win.OpenFile();
+                    StreamWriter streamWriter = new StreamWriter(fileStream);
+                    
+                    streamWriter.WriteLine(NumGen_Box.Text);//
+                    streamWriter.WriteLine(archive_boardOrder(Button_list));
+                    streamWriter.Close();
+                                                            
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+        public string archive_boardOrder(List<Button> button_list)
+        {
+            string Sequence = "" ;
+            foreach (Button button in button_list)
+            {
+                switch (button.Text)
+                {
+                    case "X": // ' ' represent a UTF 16 char , the upperlevel X is not UTF 16
+                        Sequence = Sequence + 'x' ;
+                        break;
+                    case "O":
+                        Sequence = Sequence + 'o';
+                        break;
+                    default:
+                        Sequence = Sequence + 'n';
+                        break;
+                }
+            }
+            return Sequence;
+        }
+
+        private void NumGen_Box_TextChanged(object sender, EventArgs e)
+
+        {
+
+        }
+
+        private void LOAD_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog win = new OpenFileDialog();
+            win.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            win.InitialDirectory = "C:\\Users\\sirapophuangwilai\\Desktop\\My_Dotnet\\Dynamic_UI_gen_demo\\SAVE";
+            win.FilterIndex = 2;
+            win.RestoreDirectory = true;
+            if (win.ShowDialog() == DialogResult.OK)
+            {
+                Stream fileStream = win.OpenFile() ;
+                StreamReader reader = new StreamReader(fileStream);
+                try
+                {
+                    
+                    string[] temp = new string[4] ;
+                    int counter = 0;
+                    while (reader.Peek() >= 0)
+                    {
+                        temp[counter] = reader.ReadLine() ;
+                        counter++;
+                    }
+                    // 0 for field size
+                    NumGen_Box.Text = temp[0];
+                    Generate_Fuction();
+                    // 1 for X O sequence
+                    assigned_OX(temp[1],Button_list);
+                    //List_Count.Text = temp[1];
+
+                }
+                catch (System.Exception ex)
+                {
+                    MessageBox.Show("Wrong format save file", "Error",MessageBoxButtons.OK);
+                }
+
+            } 
+
+        }
+        void assigned_OX(string Sequence , List<Button> button_list)
+        {
+            int idx_sequence = 0;
+            Button temp;
+            for (int Row = 0; Row < k; Row++)
+            {
+                for (int Coloumn = 0; Coloumn < k; Coloumn++)
+                {
+                    switch(Sequence[idx_sequence])
+                    
+                    {
+                        case 'x':
+                            temp = button_list[idx_sequence];
+                            temp.Text = "X";
+                            break;
+                        case 'o':
+                            temp = button_list[idx_sequence];
+                            temp.Text = "O";
+                            break;
+
+                        default:
+                            temp = button_list[idx_sequence];
+                            temp.Text = "";
+                            break;
+                    }
+                    idx_sequence++;
+                }
+            }
+        }
+
+
+    }// end program class
+}
